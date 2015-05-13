eeddb7454cc684d0e33309d079fc4ffb3654c7ef
Jake VanAdrighem <jvanadrighem@gmail.com>
2015-05-13 19:47:11 -0400

    Added metadata to pipeline as well as manager for the SAIO install.

--- swift/common/manager.py
+++ swift/common/manager.py
@@ -35,9 +35,10 @@ ALL_SERVERS = ['account-auditor', 'account-server', 'container-auditor',
                'container-updater', 'object-auditor', 'object-server',
                'object-expirer', 'object-replicator',
                'object-reconstructor', 'object-updater',
-               'proxy-server', 'account-replicator', 'account-reaper']
+               'proxy-server', 'account-replicator', 'account-reaper',
+               'metadata-server']
 MAIN_SERVERS = ['proxy-server', 'account-server', 'container-server',
-                'object-server']
+                'object-server', 'metadata-server']
 REST_SERVERS = [s for s in ALL_SERVERS if s not in MAIN_SERVERS]
 GRACEFUL_SHUTDOWN_SERVERS = MAIN_SERVERS + ['auth-server']
 START_ONCE_SERVERS = REST_SERVERS
